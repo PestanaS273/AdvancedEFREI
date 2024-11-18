d3f1696ac613f9f0125e1bc8ef6c1472fbe94164
SEBASTIAN <spestanam273@gmail.com>
2024-11-18 12:48:36 +0100

    update

--- APFRONT/src/main.js
+++ APFRONT/src/main.js
@@ -1,12 +1,12 @@
-import './assets/index.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
+import './style.css'
 import router from './router'
-
+import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(router)
+
+
 
 app.mount('#app')
