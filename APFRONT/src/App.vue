50afd7579c7ac526a791d02af364c3c295d22ad7
SEBASTIAN <spestanam273@gmail.com>
2024-11-19 10:15:49 +0100

    navbar udpate

--- APFRONT/src/App.vue
+++ APFRONT/src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <BaseNav />
-  <RouterView />
+<div id="app">
+  <BaseNav v-if="showNav" />
+  <router-view />
+</div>
 </template>
 
-<script setup>
+<script>
+import { mapState } from 'vuex';
+import BaseNav from './components/BaseNav.vue';
 
-import BaseNav from './components/BaseNav.vue'
-
-
+export default {
+components: {
+  BaseNav
+},
+computed: {
+  showNav() {
+    return this.$route.name !== 'login';
+  }
+}
+};
 </script>
 
+<style>
+</style>
